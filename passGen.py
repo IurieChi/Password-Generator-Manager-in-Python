6aa42b1a65d7e9b8f046eeaeec05aadb2f8d50cd
IurieChi <chigaiiura@yahoo.com>
2023-04-17 11:15:02 +0200

    generate password and encrypt + decryp generated passwor from file functions done

--- passGen.py
+++ passGen.py
@@ -6,6 +6,7 @@ import pyperclip as clipboard
 from cryptography.fernet import Fernet
 
 key = b'4b6LWAOo3Y1mgjTIjOMSAdz2LPaKFNvCfdKv1Vzg-Lc='
+token = Fernet(key)
 
 class MainWindow:
     #use constructor
@@ -84,31 +85,33 @@ class MainWindow:
         self.textBox.insert(1.0, genetated_password)
         self.textBox.config(state='disabled')
         #encrypt generated password and save it on file
-        f = Fernet(key)
-        encrypted = f.encrypt(genetated_password.encode())
-        print(type(key))
-        with open('password.txt','a') as file: 
-            file.write(str(encrypted)+'\n') #add password to file
+        
+        encrypted = token.encrypt(genetated_password.encode())
+        with open('password.txt','ab') as file: 
+            file.write(encrypted + b'\n') #add password to file
 
 
     def getHistory(self):
         self.textBox.config(state='normal')
         self.textBox.delete(1.0, 'end')
-        f= Fernet(key)
-        with open('password.txt','rb')as r:
-            readEncrypted = bytes(r.read())
-        decrypt = f.decrypt(readEncrypted).decode()
-        self.textBox.insert(1.0, decrypt)
+        
+        with open('password.txt','r')as text:
+            for line in text:
+                if line != '\n':
+                    readEncrypted = token.decrypt(line.encode())
+                    self.textBox.insert(1.0, readEncrypted)
+                    self.textBox.insert(1.0,'\n')
         self.textBox.config(state='disabled')
     
     def copyPassword(self):
         #message 
         msg = 'Password copied succesfuly'
-        msgno = 'First generate password'
+        msg_generate = 'First generate password'
 
         self.textBox.config(state='normal')
         if self.textBox.get(1.0,'end')=='\n':
-           messagebox.showwarning('Warning',msgno)
+           messagebox.showwarning('Warning',msg_generate)
+
         elif len(self.textBox.get(1.0,'end'))>=1 and len(self.textBox.get(1.0,'end'))<=12:
            #to copy password need to: pip install pyperclip then import pyperclip 
             clipboard.copy(self.textBox.get(1.0,'end'))
@@ -116,9 +119,10 @@ class MainWindow:
             self.textBox.delete(1.0,'end')
         else:
             with open('password.txt','r') as text:
-                for line in text: # read all file and copy just last password from for iteration 
-                    pass
-                clipboard.copy(line)
+                for line in text:# read all file and copy just last password from for iteration 
+                    if line == '\n':
+                        readEncrypted = token.decrypt(line.encode()) 
+                        clipboard.copy(readEncrypted)
                 messagebox.showinfo('message', msg)
                 self.textBox.delete(1.0,'end')
         self.textBox.config(state='disabled')
